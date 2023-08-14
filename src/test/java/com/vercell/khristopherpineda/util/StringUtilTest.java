7faba149e48289e69cbbf9b4939e75f963705b1e
Khristopher Pineda <khristopher.pineda@aforecoppel.com>
2023-08-14 15:32:38 -0700

    test organization 6/24

--- src/test/java/com/vercell/khristopherpineda/util/StringUtilTest.java
+++ src/test/java/com/vercell/khristopherpineda/util/StringUtilTest.java
@@ -6,8 +6,22 @@ import org.junit.Test;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat() {
-        Assert.assertEquals("somethingsomethingsomething", StringUtil.repeat("something", 3));
+    public void repeat_string_once() {
         Assert.assertEquals("something", StringUtil.repeat("something", 1));
+    }
+
+    @Test
+    public void repeat_string_multiple_times() {
+        Assert.assertEquals("somethingsomethingsomething", StringUtil.repeat("something", 3));
+    }
+
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals("", StringUtil.repeat("something", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        Assert.assertEquals("", StringUtil.repeat("something", -1));
     }
 }
